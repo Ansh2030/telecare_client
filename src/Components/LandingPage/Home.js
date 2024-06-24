34ae204241fbc40bf6dadc510b14ec7b34f9fd5f
unknown <ankursharma9.10.2003@gmail.com>
2024-06-24 22:27:48 +0530

    Landing Page

---
+++ src/Components/LandingPage/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import AboutUs from './AboutUs';
+import HomeCircles from './HomeCircles';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <HomeCircles />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default Landing;
+
