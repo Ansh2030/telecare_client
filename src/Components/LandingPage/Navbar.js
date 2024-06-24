7a2facbd653cdd4b8908588a3220c6209fc4c97e
unknown <ankursharma9.10.2003@gmail.com>
2024-06-24 23:09:14 +0530

    L

--- src/Components/LandingPage/Navbar.js
+++ src/Components/LandingPage/Navbar.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+import { FiMenu } from "react-icons/fi";
+import { RxCross1 } from "react-icons/rx";
 
-function Navbar() {
+const Navbar = () => {
+  const [iconActive, setIconActive] = useState(false);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header>
+      <nav className={iconActive ? "nav-active" : ""}>
+        <h2 className="nav-logo">
+          <NavLink to={"/"}>HealthBooker</NavLink>
+        </h2>
+        <ul className="nav-links">
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/doctors"}>Doctors</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/appointments"}>Appointments</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/notifications"}>Notifications</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/applyfordoctor"}>Apply for doctor</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/#contact"}>Contact Us</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/profile"}>Profile</NavLink>
+          </li>
+          <li>
+            <NavLink className="btn" to={"/login"}>
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="btn" to={"/register"}>
+              Register
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="menu-icons">
+        {!iconActive ? (
+          <FiMenu
+            className="menu-open"
+            onClick={() => {
+              setIconActive(true);
+            }}
+          />
+        ) : (
+          <RxCross1
+            className="menu-close"
+            onClick={() => {
+              setIconActive(false);
+            }}
+          />
+        )}
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
